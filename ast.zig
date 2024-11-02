2e2404ab5b58880ba2eea655b2291e21274b9892
Luca Schneider <luca.schneider@gmx.ch>
2024-11-02 11:06:36 +0100

    parsing expression works

--- ast.zig
+++ ast.zig
@@ -10,26 +10,68 @@ const Node = union(enum) {
             inline else => |impl| impl.tokenLiteral(),
         };
     }
+
+    pub fn string(self: *const Node) []const u8 {
+        return switch (self.*) {
+            inline else => |impl| impl.string(),
+        };
+    }
 };
 
 pub const Statement = union(enum) {
     let_statement: LetStatement,
     return_statement: ReturnStatement,
+    expression_statement: ExpressionStatement,
 
     pub fn tokenLiteral(self: *const Statement) []const u8 {
         return switch (self.*) {
             inline else => |impl| impl.tokenLiteral(),
         };
     }
+
+    pub fn string(self: *const Statement, allocator: std.mem.Allocator) ![]const u8 {
+        return switch (self.*) {
+            inline else => |impl| try impl.string(allocator),
+        };
+    }
 };
 
 pub const Expression = union(enum) {
     identifier: Identifier,
+    integer_literal: IntegerLiteral,
+    prefix_expression: PrefixExpression,
+    infix_expression: InfixExpression,
 
     pub fn tokenLiteral(self: *const Expression) []const u8 {
         return switch (self.*) {
             inline else => |impl| impl.tokenLiteral(),
         };
+    }
+
+    pub fn string(self: *const Expression, allocator: std.mem.Allocator) anyerror![]const u8 {
+        return switch (self.*) {
+            inline else => |impl| impl.string(allocator),
+        };
+    }
+};
+
+pub const ExpressionStatement = struct {
+    token: Token,
+    expression: Expression,
+
+    pub fn statmentNode() void {}
+    pub fn tokenLiteral(es: *const ExpressionStatement) []const u8 {
+        return es.token.literal;
+    }
+    pub fn string(es: *const ExpressionStatement, allocator: std.mem.Allocator) ![]const u8 {
+        var output = std.ArrayList(u8).init(allocator);
+        errdefer output.deinit();
+
+        const expr_str = try es.expression.string(allocator);
+        defer allocator.free(expr_str);
+        try output.appendSlice(expr_str);
+
+        return output.toOwnedSlice();
     }
 };
 
@@ -39,6 +81,129 @@ pub const Identifier = struct {
 
     pub fn tokenLiteral(i: *const Identifier) []const u8 {
         return i.token.literal;
+    }
+
+    pub fn string(i: *const Identifier, allocator: std.mem.Allocator) ![]const u8 {
+        return allocator.dupe(u8, i.value);
+    }
+};
+
+pub const IntegerLiteral = struct {
+    token: Token,
+    value: i64,
+
+    pub fn tokenLiteral(i: *const IntegerLiteral) []const u8 {
+        return i.token.literal;
+    }
+
+    pub fn string(i: *const IntegerLiteral, allocator: std.mem.Allocator) ![]const u8 {
+        return allocator.dupe(u8, i.token.literal);
+    }
+};
+
+pub const PrefixExpression = struct {
+    token: Token,
+    operator: []const u8,
+    right: *Expression,
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator) !PrefixExpression {
+        return PrefixExpression{
+            .token = undefined,
+            .operator = undefined,
+            .right = try allocator.create(Expression),
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: *PrefixExpression) void {
+        // First recursively free any nested expressions
+        switch (self.right.*) {
+            .prefix_expression => |*pe| pe.deinit(),
+            .infix_expression => |*ie| ie.deinit(),
+            else => {},
+        }
+        // Then free the right expression pointer itself
+        self.allocator.destroy(self.right);
+    }
+
+    pub fn tokenLiteral(pe: *const PrefixExpression) []const u8 {
+        return pe.token.literal;
+    }
+
+    pub fn string(pe: *const PrefixExpression, allocator: std.mem.Allocator) ![]const u8 {
+        var out = std.ArrayList(u8).init(allocator);
+        errdefer out.deinit();
+
+        try out.appendSlice("(");
+        try out.appendSlice(pe.operator);
+
+        const right_str = try pe.right.string(allocator);
+        defer allocator.free(right_str);
+        try out.appendSlice(right_str);
+
+        try out.appendSlice(")");
+        return out.toOwnedSlice();
+    }
+};
+
+pub const InfixExpression = struct {
+    token: Token,
+    left: *Expression,
+    operator: []const u8,
+    right: *Expression,
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator) !InfixExpression {
+        return InfixExpression{
+            .token = undefined,
+            .left = try allocator.create(Expression),
+            .operator = undefined,
+            .right = try allocator.create(Expression),
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: *InfixExpression) void {
+        // First recursively free any nested expressions
+        switch (self.left.*) {
+            .prefix_expression => |*pe| pe.deinit(),
+            .infix_expression => |*ie| ie.deinit(),
+            else => {},
+        }
+        switch (self.right.*) {
+            .prefix_expression => |*pe| pe.deinit(),
+            .infix_expression => |*ie| ie.deinit(),
+            else => {},
+        }
+        // Then free the expression pointers themselves
+        self.allocator.destroy(self.left);
+        self.allocator.destroy(self.right);
+    }
+
+    pub fn tokenLiteral(self: *const InfixExpression) []const u8 {
+        return self.token.literal;
+    }
+
+    pub fn string(self: *const InfixExpression, allocator: std.mem.Allocator) ![]const u8 {
+        var out = std.ArrayList(u8).init(allocator);
+        errdefer out.deinit();
+
+        const left_str = try self.left.string(allocator);
+        defer allocator.free(left_str);
+
+        const right_str = try self.right.string(allocator);
+        defer allocator.free(right_str);
+
+        try out.appendSlice("(");
+        try out.appendSlice(left_str);
+        try out.appendSlice(" ");
+        try out.appendSlice(self.operator);
+        try out.appendSlice(" ");
+        try out.appendSlice(right_str);
+        try out.appendSlice(")");
+
+        return out.toOwnedSlice();
     }
 };
 
@@ -65,6 +230,18 @@ pub const LetStatement = struct {
     pub fn tokenLiteral(ls: *const LetStatement) []const u8 {
         return ls.token.literal;
     }
+
+    pub fn string(ls: *const LetStatement, allocator: std.mem.Allocator) ![]const u8 {
+        var out = std.ArrayList(u8).init(allocator);
+        errdefer out.deinit();
+
+        try out.appendSlice("let ");
+        try out.appendSlice(ls.name.value);
+        try out.appendSlice(" = ");
+        try out.appendSlice(ls.value.tokenLiteral());
+        try out.appendSlice(";");
+        return out.toOwnedSlice();
+    }
 };
 
 pub const ReturnStatement = struct {
@@ -87,14 +264,25 @@ pub const ReturnStatement = struct {
     pub fn tokenLiteral(rs: *const ReturnStatement) []const u8 {
         return rs.token.literal;
     }
+
+    pub fn string(rs: *const ReturnStatement, allocator: std.mem.Allocator) ![]const u8 {
+        var out = std.ArrayList(u8).init(allocator);
+        defer out.deinit();
+        try out.appendSlice("return ");
+        try out.appendSlice(rs.returnValue.tokenLiteral());
+        try out.appendSlice(";");
+        return out.toOwnedSlice();
+    }
 };
 
 pub const Program = struct {
     statements: std.ArrayList(Statement),
+    allocator: std.mem.Allocator,
 
     pub fn init(allocator: std.mem.Allocator) !Program {
         return .{
             .statements = std.ArrayList(Statement).init(allocator),
+            .allocator = allocator,
         };
     }
 
@@ -103,6 +291,17 @@ pub const Program = struct {
             switch (stmt.*) {
                 .let_statement => |*ls| ls.deinit(),
                 .return_statement => |*rs| rs.deinit(),
+                .expression_statement => |*es| {
+                    switch (es.expression) {
+                        .prefix_expression => |*pe| {
+                            pe.deinit();
+                        },
+                        .infix_expression => |*ie| {
+                            ie.deinit();
+                        },
+                        else => {},
+                    }
+                },
             }
         }
         self.statements.deinit();
@@ -115,4 +314,117 @@ pub const Program = struct {
             return "";
         }
     }
+
+    pub fn string(p: *const Program) ![]const u8 {
+        var output = std.ArrayList(u8).init(p.allocator);
+        errdefer output.deinit();
+
+        // Iterate through statements and append their string representations
+        for (p.statements.items) |statement| {
+            if (statement.string(p.allocator)) |stmt_str| {
+                defer p.allocator.free(stmt_str);
+                try output.appendSlice(stmt_str);
+            } else |err| {
+                output.deinit();
+                return err;
+            }
+        }
+
+        return output.toOwnedSlice();
+    }
 };
+
+test "test serializing let statement" {
+    const allocator = std.testing.allocator;
+
+    // Initialize Program first since it should be deallocated last
+    var program = Program{
+        .statements = std.ArrayList(Statement).init(allocator),
+        .allocator = allocator,
+    };
+    defer program.deinit(); // This will clean up everything
+
+    // Create identifier
+    const identifier = try allocator.create(Identifier);
+    identifier.* = Identifier{
+        .token = .{
+            .type = .IDENT,
+            .literal = "myVar",
+        },
+        .value = "myVar",
+    };
+
+    // Create expression
+    const value = try allocator.create(Expression);
+    value.* = Expression{
+        .identifier = .{
+            .token = .{
+                .type = .IDENT,
+                .literal = "anotherVar",
+            },
+            .value = "anotherVar",
+        },
+    };
+
+    // Create and append the statement
+    try program.statements.append(Statement{
+        .let_statement = LetStatement{
+            .name = identifier,
+            .token = .{
+                .type = .LET,
+                .literal = "let",
+            },
+            .value = value,
+            .allocator = allocator,
+        },
+    });
+
+    // Test the string output
+    const result = try program.string();
+    defer allocator.free(result);
+
+    try std.testing.expectEqualStrings("let myVar = anotherVar;", result);
+}
+
+test "test serializing return statement" {
+    const allocator = std.testing.allocator;
+
+    // Initialize Program first since it should be deallocated last
+    var program = Program{
+        .statements = std.ArrayList(Statement).init(allocator),
+        .allocator = allocator,
+    };
+    defer program.deinit(); // This will clean up everything
+
+    // Create expression
+    const value = try allocator.create(Expression);
+    value.* = Expression{
+        .identifier = .{
+            .token = .{
+                .type = .IDENT,
+                .literal = "myVal",
+            },
+            .value = "myVal",
+        },
+    };
+
+    // Create and append the statement
+    try program.statements.append(Statement{
+        .return_statement = ReturnStatement{
+            // .name = identifier,
+            .token = .{
+                .type = .RETURN,
+                .literal = "return",
+            },
+            // .value = value,
+            .returnValue = value,
+            .allocator = allocator,
+        },
+    });
+
+    // Test the string output
+    const result = try program.string();
+    defer allocator.free(result);
+
+    try std.testing.expectEqualStrings("return myVal;", result);
+}
--- parser.zig
+++ parser.zig
@@ -4,12 +4,43 @@ const TokenType = @import("token.zig").TokenType;
 const ast = @import("ast.zig");
 const std = @import("std");
 
-pub const ParseError = error{
-    UnexpectedToken,
-    InvalidIdentifier,
-    MissingSemicolon,
-    OutOfMemory,
+pub const ParseError = error{ UnexpectedToken, InvalidIdentifier, MissingSemicolon, OutOfMemory, Overflow, InvalidCharacter };
+
+pub const prefixParseFn = *const fn (*Parser) ParseError!ast.Expression;
+pub const infixParseFn = *const fn (*Parser, ast.Expression) ParseError!ast.Expression;
+
+const Priority = enum(u4) {
+    LOWEST = 1,
+    EQUALS = 2, // ==
+    LESSGREATER = 3, // > or <
+    SUM = 4, // +
+    PRODUCT = 5, // *
+    PREFIX = 6, // -X or !X
+    CALL = 7, // myFunction(X)
 };
+
+const precedences = struct {
+    fn init() type {
+        const MapType = struct {
+            const map = std.static_string_map.StaticStringMap(Priority).initComptime(.{
+                .{ TokenType.EQ.toString(), .EQUALS },
+                .{ TokenType.NOT_EQ.toString(), .EQUALS },
+                .{ TokenType.LT.toString(), .LESSGREATER },
+                .{ TokenType.GT.toString(), .LESSGREATER },
+                .{ TokenType.PLUS.toString(), .SUM },
+                .{ TokenType.MINUS.toString(), .SUM },
+                .{ TokenType.SLASH.toString(), .PRODUCT },
+                .{ TokenType.ASTERISK.toString(), .PRODUCT },
+                .{ TokenType.LPAREN.toString(), .CALL },
+            });
+
+            pub fn get(token_type: TokenType) Priority {
+                return map.get(token_type.toString()) orelse .LOWEST;
+            }
+        };
+        return MapType;
+    }
+}.init();
 
 const Parser = struct {
     allocator: std.mem.Allocator,
@@ -17,6 +48,8 @@ const Parser = struct {
     curr_token: Token,
     peek_token: Token,
     errors: std.ArrayList([]const u8),
+    prefix_parse_fns: std.AutoHashMap(TokenType, prefixParseFn),
+    infix_parse_fns: std.AutoHashMap(TokenType, infixParseFn),
 
     pub fn init(allocator: std.mem.Allocator, lexer: *Lexer) Parser {
         var parser = Parser{
@@ -25,7 +58,23 @@ const Parser = struct {
             .curr_token = undefined,
             .peek_token = undefined,
             .errors = std.ArrayList([]const u8).init(allocator),
+            .prefix_parse_fns = std.AutoHashMap(TokenType, prefixParseFn).init(allocator),
+            .infix_parse_fns = std.AutoHashMap(TokenType, infixParseFn).init(allocator),
         };
+
+        parser.registerPrefix(.IDENT, @This().parseIdentifier) catch {};
+        parser.registerPrefix(.INT, @This().parseIntegerLiteral) catch {};
+        parser.registerPrefix(.BANG, @This().parsePrefixExpression) catch {};
+        parser.registerPrefix(.MINUS, @This().parsePrefixExpression) catch {};
+
+        parser.registerInfix(.PLUS, @This().parseInfixExpression) catch {};
+        parser.registerInfix(.MINUS, @This().parseInfixExpression) catch {};
+        parser.registerInfix(.SLASH, @This().parseInfixExpression) catch {};
+        parser.registerInfix(.ASTERISK, @This().parseInfixExpression) catch {};
+        parser.registerInfix(.EQ, @This().parseInfixExpression) catch {};
+        parser.registerInfix(.NOT_EQ, @This().parseInfixExpression) catch {};
+        parser.registerInfix(.LT, @This().parseInfixExpression) catch {};
+        parser.registerInfix(.GT, @This().parseInfixExpression) catch {};
 
         // Read two tokens to initialize curr_token and peek_token
         parser.nextToken();
@@ -39,6 +88,8 @@ const Parser = struct {
             self.allocator.free(msg);
         }
         self.errors.clearAndFree();
+        self.prefix_parse_fns.deinit();
+        self.infix_parse_fns.deinit();
     }
 
     fn peekError(self: *Parser, expected: TokenType) !void {
@@ -77,7 +128,9 @@ const Parser = struct {
             .RETURN => .{
                 .return_statement = try self.parseReturnStatement(),
             },
-            else => null,
+            else => .{
+                .expression_statement = try self.parseExpressionStatement(),
+            },
         };
     }
 
@@ -114,6 +167,86 @@ const Parser = struct {
         return stmt;
     }
 
+    fn parseExpressionStatement(self: *Parser) ParseError!ast.ExpressionStatement {
+        const stmt = .{
+            .expression = try self.parseExpression(.LOWEST),
+            .token = undefined,
+        };
+
+        if (self.peek_token.type == .SEMICOLON) {
+            self.nextToken();
+        }
+
+        return stmt;
+    }
+
+    fn parseExpression(self: *Parser, precedence: Priority) ParseError!ast.Expression {
+        const prefix = self.prefix_parse_fns.get(self.curr_token.type);
+        if (prefix == null) {
+            std.debug.print("no prefix parse function for {s} found\n", .{self.curr_token.type.toString()});
+        }
+        var left_exp = try prefix.?(self);
+
+        while (self.peek_token.type != .SEMICOLON and @intFromEnum(precedence) < @intFromEnum(self.peekPrecedence())) {
+            const infix = self.infix_parse_fns.get(self.peek_token.type);
+            if (infix == null) {
+                return left_exp;
+            }
+
+            self.nextToken();
+            left_exp = try infix.?(self, left_exp);
+        }
+
+        return left_exp;
+    }
+
+    fn parseIdentifier(self: *Parser) ParseError!ast.Expression {
+        return .{ .identifier = .{
+            .token = self.curr_token,
+            .value = self.curr_token.literal,
+        } };
+    }
+
+    fn parseIntegerLiteral(self: *Parser) ParseError!ast.Expression {
+        const lit = try std.fmt.parseInt(i64, self.curr_token.literal, 10);
+        return .{ .integer_literal = .{
+            .token = self.curr_token,
+            .value = lit,
+        } };
+    }
+
+    fn parsePrefixExpression(self: *Parser) ParseError!ast.Expression {
+        var prefixExpression = try ast.PrefixExpression.init(self.allocator);
+        errdefer prefixExpression.deinit();
+
+        prefixExpression.token = self.curr_token;
+        prefixExpression.operator = self.curr_token.literal;
+
+        self.nextToken();
+
+        const right_expr = try self.parseExpression(.PREFIX);
+        prefixExpression.right.* = right_expr;
+
+        return .{ .prefix_expression = prefixExpression };
+    }
+
+    fn parseInfixExpression(self: *Parser, left: ast.Expression) ParseError!ast.Expression {
+        var infixExpression = try ast.InfixExpression.init(self.allocator);
+        errdefer infixExpression.deinit();
+
+        infixExpression.token = self.curr_token;
+        infixExpression.operator = self.curr_token.literal;
+        infixExpression.left.* = left;
+
+        const precedence = self.curPrecedence();
+        self.nextToken();
+
+        const right_expr = try self.parseExpression(precedence);
+        infixExpression.right.* = right_expr;
+
+        return .{ .infix_expression = infixExpression };
+    }
+
     fn expectPeek(self: *Parser, expected: TokenType) ParseError!void {
         if (self.peek_token.type != expected) {
             try self.peekError(expected);
@@ -133,6 +266,22 @@ const Parser = struct {
 
     fn checkToken(self: Parser, tok_type: TokenType) bool {
         return self.curr_token.type == tok_type;
+    }
+
+    fn registerPrefix(self: *Parser, token_type: TokenType, f: prefixParseFn) !void {
+        try self.prefix_parse_fns.put(token_type, f);
+    }
+
+    fn registerInfix(self: *Parser, token_type: TokenType, f: infixParseFn) !void {
+        try self.infix_parse_fns.put(token_type, f);
+    }
+
+    fn peekPrecedence(self: *Parser) Priority {
+        return precedences.get(self.peek_token.type);
+    }
+
+    fn curPrecedence(self: *Parser) Priority {
+        return precedences.get(self.curr_token.type);
     }
 
     // Testing
@@ -159,6 +308,16 @@ const Parser = struct {
                 }
             }
             try std.testing.expectEqual(@as(usize, 0), parser.errors.items.len);
+        }
+
+        fn testIntegerLiteral(il: ast.Expression, value: i64) !void {
+            const int_lit = il.integer_literal;
+
+            var b: [10]u8 = undefined;
+            const x = try std.fmt.bufPrint(&b, "{d}", .{value});
+
+            try std.testing.expectEqual(value, int_lit.value);
+            try std.testing.expectEqualStrings(x, int_lit.tokenLiteral());
         }
     };
 };
@@ -218,5 +377,164 @@ test "test return statements" {
     for (0..program.statements.items.len) |i| {
         const stmt = &program.statements.items[i];
         try std.testing.expectEqualStrings("return", @as(*ast.ReturnStatement, @ptrCast(stmt)).tokenLiteral());
+    }
+}
+
+test "test identifier expression" {
+    const input = "foobar;";
+    var lexer = Lexer.init(input);
+    const allocator = std.testing.allocator;
+    var parser = Parser.init(allocator, &lexer);
+    defer parser.deinit();
+
+    var program = try parser.parseProgram();
+    defer program.deinit();
+
+    try Parser.Testing.checkParserErrors(&parser);
+
+    try std.testing.expectEqual(@as(usize, 1), program.statements.items.len);
+
+    for (0..program.statements.items.len) |i| {
+        const stmt = &program.statements.items[i];
+        const expr_stmt = @as(*ast.ExpressionStatement, @ptrCast(stmt));
+        const ident = expr_stmt.expression.identifier;
+
+        try std.testing.expectEqualStrings("foobar", ident.value);
+        try std.testing.expectEqualStrings("foobar", ident.tokenLiteral());
+    }
+}
+
+test "test integer literal expression" {
+    const input = "5;";
+    var lexer = Lexer.init(input);
+    const allocator = std.testing.allocator;
+    var parser = Parser.init(allocator, &lexer);
+    defer parser.deinit();
+
+    var program = try parser.parseProgram();
+    defer program.deinit();
+
+    try Parser.Testing.checkParserErrors(&parser);
+
+    try std.testing.expectEqual(@as(usize, 1), program.statements.items.len);
+
+    for (0..program.statements.items.len) |i| {
+        const stmt = &program.statements.items[i];
+        const expr_stmt = @as(*ast.ExpressionStatement, @ptrCast(stmt));
+        const ident = expr_stmt.expression.integer_literal;
+
+        try std.testing.expectEqual(5, ident.value);
+        try std.testing.expectEqualStrings("5", ident.tokenLiteral());
+    }
+}
+
+test "test parsing prefix expression" {
+    const Case = struct {
+        input: []const u8,
+        expected_operator: []const u8,
+        expected_value: i64,
+    };
+    const input = [_]Case{ .{ .input = "!5;", .expected_operator = "!", .expected_value = 5 }, .{ .input = "-15;", .expected_operator = "-", .expected_value = 15 } };
+    for (input) |tt| {
+        var lexer = Lexer.init(tt.input);
+        const allocator = std.testing.allocator;
+        var parser = Parser.init(allocator, &lexer);
+        defer parser.deinit();
+
+        var program = try parser.parseProgram();
+        defer program.deinit();
+
+        try Parser.Testing.checkParserErrors(&parser);
+
+        try std.testing.expectEqual(@as(usize, 1), program.statements.items.len);
+
+        for (0..program.statements.items.len) |i| {
+            const stmt = &program.statements.items[i];
+            const expr_stmt = @as(*ast.ExpressionStatement, @ptrCast(stmt));
+            const prfx_expr = expr_stmt.expression.prefix_expression;
+
+            try std.testing.expectEqual(tt.expected_operator, prfx_expr.operator);
+            try Parser.Testing.testIntegerLiteral(prfx_expr.right.*, tt.expected_value);
+        }
+    }
+}
+
+test "test parsing infix expressions" {
+    const Case = struct {
+        input: []const u8,
+        expected_left_value: i64,
+        expected_operator: []const u8,
+        expected_right_value: i64,
+    };
+
+    const input = [_]Case{
+        .{ .input = "5 + 5;", .expected_left_value = 5, .expected_operator = "+", .expected_right_value = 5 },
+        .{ .input = "5 - 5;", .expected_left_value = 5, .expected_operator = "-", .expected_right_value = 5 },
+        .{ .input = "5 * 5;", .expected_left_value = 5, .expected_operator = "*", .expected_right_value = 5 },
+        .{ .input = "5 / 5;", .expected_left_value = 5, .expected_operator = "/", .expected_right_value = 5 },
+        .{ .input = "5 > 5;", .expected_left_value = 5, .expected_operator = ">", .expected_right_value = 5 },
+        .{ .input = "5 < 5;", .expected_left_value = 5, .expected_operator = "<", .expected_right_value = 5 },
+        .{ .input = "5 == 5;", .expected_left_value = 5, .expected_operator = "==", .expected_right_value = 5 },
+        .{ .input = "5 != 5;", .expected_left_value = 5, .expected_operator = "!=", .expected_right_value = 5 },
+    };
+    for (input) |tt| {
+        var lexer = Lexer.init(tt.input);
+        const allocator = std.testing.allocator;
+        var parser = Parser.init(allocator, &lexer);
+        defer parser.deinit();
+
+        var program = try parser.parseProgram();
+        defer program.deinit();
+
+        try Parser.Testing.checkParserErrors(&parser);
+
+        try std.testing.expectEqual(@as(usize, 1), program.statements.items.len);
+
+        for (0..program.statements.items.len) |i| {
+            const stmt = &program.statements.items[i];
+            const expr_stmt = @as(*ast.ExpressionStatement, @ptrCast(stmt));
+            const infx_expr = expr_stmt.expression.infix_expression;
+
+            try Parser.Testing.testIntegerLiteral(infx_expr.left.*, tt.expected_left_value);
+            try std.testing.expectEqual(tt.expected_operator, infx_expr.operator);
+            try Parser.Testing.testIntegerLiteral(infx_expr.right.*, tt.expected_right_value);
+        }
+    }
+}
+
+test "test parsing operator precedence" {
+    const Case = struct {
+        input: []const u8,
+        expected: []const u8,
+    };
+
+    const input = [_]Case{
+        .{ .input = "-a * b", .expected = "((-a) * b)" },
+        .{ .input = "!-a", .expected = "(!(-a))" },
+        .{ .input = "a + b + c", .expected = "((a + b) + c)" },
+        .{ .input = "a + b - c", .expected = "((a + b) - c)" },
+        .{ .input = "a * b * c", .expected = "((a * b) * c)" },
+        .{ .input = "a * b / c", .expected = "((a * b) / c)" },
+        .{ .input = "a + b / c", .expected = "(a + (b / c))" },
+        .{ .input = "a + b * c + d / e - f", .expected = "(((a + (b * c)) + (d / e)) - f)" },
+        .{ .input = "3 + 4; -5 * 5", .expected = "(3 + 4)((-5) * 5)" },
+        .{ .input = "5 > 4 == 3 < 4", .expected = "((5 > 4) == (3 < 4))" },
+        .{ .input = "5 < 4 != 3 > 4", .expected = "((5 < 4) != (3 > 4))" },
+        .{ .input = "3 + 4 * 5 == 3 * 1 + 4 * 5", .expected = "((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))" },
+    };
+    for (input) |tt| {
+        var lexer = Lexer.init(tt.input);
+        const allocator = std.testing.allocator;
+        var parser = Parser.init(allocator, &lexer);
+        defer parser.deinit();
+
+        var program = try parser.parseProgram();
+        defer program.deinit();
+
+        try Parser.Testing.checkParserErrors(&parser);
+
+        const result = try program.string();
+        defer allocator.free(result);
+        try std.testing.expectEqualStrings(tt.expected, result);
     }
 }
